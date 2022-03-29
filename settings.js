2488429700ae991d0f328a5c0308d80a85130941
EsHayes <92333365+EsHayes@users.noreply.github.com>
2022-03-29 22:39:28 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,7 @@
 function confirmed() {
- 		var _soulmates = document.getElementById("id_1");
+		var _soulmates = document.getElementById("id_1");
 		var _magic = document.getElementById("id_2");
+		
  		_soulmates.disabled = true; _magic.disabled = true;
 		$enabledsoulmates = _soulmates.checked;
 		$enabledmagic = _magic.checked;
@@ -9,9 +10,10 @@ function confirmed() {
 	}
 	
 function reload() {
- 		var _soulmates = document.getElementById("id_1");
+		var _soulmates = document.getElementById("id_1");
 		var _magic = document.getElementById("id_2");
-		document.getElementById("id_1").checked = $enabledsoulmates;
-		document.getElementById("id_2").checked = $enabledmagic;
+	
+		_soulmates.checked = $enabledsoulmates;
+		_magic.checked = $enabledmagic;
 		_soulmates.disabled = true; _magic.disabled = true;
 	}
