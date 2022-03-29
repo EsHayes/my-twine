219f21774693fe6054232f174d08d63844b656d9
EsHayes <92333365+EsHayes@users.noreply.github.com>
2022-03-29 20:50:05 +0100

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,11 @@
+ function confirmed() {
+ 		var _soulmates = document.getElementById("id_1");
+		var _magic = document.getElementById("id_2");
+		
+ 		_soulmates.disabled = true; _magic.disabled = true;
+		
+		$enabledsoulmates = _soulmates.checked;
+		$enabledmagic = _magic.checked;
+		
+		// document.getElementById("debugged").innerHTML = $var + " " + $var_2; 
+}
